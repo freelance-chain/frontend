0b075a71dfebc9ae190865db7dff3bd6fd1e2b79
Barış Yesari <yesaribaris23@gmail.com>
2024-11-12 18:28:37 +0300

    proxy and tsconfig settings

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
